8213a49733e6631d736be9612194ca36f8e77276
Definitely not xl_83 <LXALT69@outlook.com>
2022-04-22 02:04:08 +0000

    ws

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require('socket.io');
-const io = new Server(server);
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "https://lx8369.github.io"
+    }
+});
 
 app.get('/', (req, res) => {
-  res.send('hello');
+    res.send('hello');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+    console.log('a user connected');
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
