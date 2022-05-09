ff4c4c3e3da12ae34faf880e95db564d6efa0dc5
Definitely not xl_83 <98920372+LX8369@users.noreply.github.com>
2022-05-09 15:13:16 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+var CryptoJS = require("crypto-js");
 const server = http.createServer(app);
 var hashedKey = "244dc524b6bba33086418c1a68cb4bd95304a2562489c6c19d5c785979f48b7f";
-var CryptoJS = require("crypto-js");
 
 const io = require("socket.io")(server, {
     cors: {
